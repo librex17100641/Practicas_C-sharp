b7120c19bc5910f3994620c1d13ea0bb13e97bfc
David Izar <librex176@gmail.com>
2022-09-08 18:14:38 -0500

    Segundo commit

---
+++ Cardinalidad.cs
@@ -0,0 +1,89 @@
+﻿
+using System;
+using static System.Console;
+public class programa_4{
+
+static void Main(){
+Boolean flag = true;
+String limite ;
+
+byte prueba = 1;
+String last;
+String letras;
+WriteLine("Introduzca el limite");
+limite= ReadLine();
+
+    while (flag){
+
+        if(!byte.TryParse(limite, out prueba)){
+            Console.WriteLine("El numero ingresado no es valido, ingrese otro");
+                limite = Console.ReadLine();
+           }else{
+               flag=false;
+
+               imprimir(limite);
+           }
+
+       
+}
+
+}
+static String numero_final (String num){
+String num_final;
+
+num_final=Convert.ToString(num[num.Length -1]);
+
+
+
+return num_final;
+
+}
+
+static String determinar (String terminacion, String num){
+String letras;
+
+if (terminacion == "1"){
+if(num=="11" || num=="111" ||num=="211"){
+    letras = "th";
+}else{letras = "st";}
+}else if(terminacion == "2"){
+
+if(num=="12" || num=="112" ||num=="212"){
+    letras = "th";
+}else{letras = "nd";}
+
+}else if(terminacion == "3"){
+
+if(num=="13" || num=="113" ||num=="213"){
+    letras = "th";
+}else{letras = "rd";}
+
+}else {
+    letras = "th";
+}
+
+
+return letras;
+}
+
+static async void imprimir(String num){
+String last, letras, impresion;
+    
+ 
+byte impr= byte.Parse(num);
+
+for (int i =1; i <= impr; i++ ){
+    impresion= ""+i;
+last= numero_final(impresion);
+ letras= determinar(last,impresion);
+    WriteLine("{0}-{1}",i, letras);
+    
+
+}
+
+
+}
+
+
+
+}
---
+++ Palindromos.cs
@@ -0,0 +1,57 @@
+﻿using System.IO;
+using static System.Convert;
+using static System.Console;
+namespace NullHandling{
+
+class programa_5{
+
+static void Main(string[]args){
+
+WriteLine("Introduzca la palabra");
+string  palabra = ReadLine();
+
+
+
+
+
+palindromo(palabra);
+
+
+}
+
+static  async void palindromo( string   pal){
+
+for (int i = 0; i <= pal.Length; i++)
+{
+    int j = i+2;
+
+    if(pal[i]==pal[j]){
+
+        string [] palin  = new string [1];
+        for (int f = 0; f <= j; f++)
+        WriteLine($"{palin[f]}");
+        {
+            
+        }
+        
+
+    }else{
+        WriteLine("No es un palindromo");
+    }
+}
+
+
+
+
+}
+
+    
+}
+
+
+}
+
+
+
+
+
